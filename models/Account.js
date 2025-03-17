7d73ac5f308b52ba0364eb7ed4d3dcd5bbb4bf9b
princemashumu <princemashumu@yahoo.com>
2025-03-17 09:37:32 +0200

    feat. i updated the account model

--- models/Account.js
+++ models/Account.js
@@ -1,14 +1,55 @@
-const { DataTypes } = require("sequelize");
-const sequelize = require("../config/database");
+const { v4: uuidv4 } = require('uuid');
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
 
-const Account = sequelize.define("Account", {
-  id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
-  userId: { type: DataTypes.UUID, allowNull: false }, // Links to the User model
-  name: { 
-    type: DataTypes.ENUM("Main", "Baby Care", "Entertainment", "Clothing", "Savings", "Pregnancy"),
-    allowNull: false 
+const Account = sequelize.define('Account', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: uuidv4,
+    primaryKey: true,
   },
-  balance: { type: DataTypes.FLOAT, defaultValue: 0.0 },
-}, { timestamps: true });
+  userId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+  accountType: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  accountNumber: {
+    type: DataTypes.STRING(10),
+    allowNull: false,
+    unique: true,
+  },
+  balance: {
+    type: DataTypes.FLOAT,
+    defaultValue: 0,
+  },
+  currency: {
+    type: DataTypes.STRING(3),
+    defaultValue: 'ZAR',
+    allowNull: false,
+  },
+  status: {
+    type: DataTypes.ENUM('active', 'inactive', 'frozen'),
+    defaultValue: 'active',
+    allowNull: false,
+  },
+  creationDate: {
+    type: DataTypes.DATEONLY,
+    defaultValue: DataTypes.NOW,
+    allowNull: false,
+  },
+  lastTransactionDate: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+  parentAccountId: {
+    type: DataTypes.UUID,
+    allowNull: true,
+  },
+}, {
+  timestamps: true,
+});
 
 module.exports = Account;
