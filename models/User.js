ad84c1a211cc9a8539a12f57ddc5f3ee14ae4086
princemashumu <princemashumu@yahoo.com>
2025-03-17 09:37:55 +0200

    feat. i updated the user model and added some more details

--- models/User.js
+++ models/User.js
@@ -1,17 +1,57 @@
-const { DataTypes } = require("sequelize");
-const sequelize = require("../config/database");
-const Account = require("./Account");
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
 
-const User = sequelize.define("User", {
-  id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
-  name: { type: DataTypes.STRING, allowNull: false },
-  email: { type: DataTypes.STRING, allowNull: false, unique: true, validate: { isEmail: true } },
-  password: { type: DataTypes.STRING, allowNull: false },
-  role: { type: DataTypes.ENUM("funder", "caregiver", "dependent"), allowNull: false },
-}, { timestamps: true });
-
-// Define associations
-User.hasMany(Account, { foreignKey: "userId", onDelete: "CASCADE" });
-Account.belongsTo(User, { foreignKey: "userId" });
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+    unique: true,
+  },
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  surname: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.ENUM('funder', 'caregiver', 'dependent'),
+    allowNull: false,
+  },
+  Idnumber: {
+    type: DataTypes.STRING(13),
+    allowNull: false,
+    unique: true,
+    validate: {
+      isNumeric: true,
+      len: [13, 13],
+    },
+  },
+  relation: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+}, {
+  timestamps: true,
+  tableName: 'Users',
+});
 
 module.exports = User;
