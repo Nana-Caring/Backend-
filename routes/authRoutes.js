105a1d7bc0d89c318d76a2cd75864a7d3ee45022
princemashumu <princemashumu@yahoo.com>
2025-03-17 09:38:56 +0200

    feat. i updated the auth routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,20 +1,37 @@
 const express = require("express");
 const { check } = require("express-validator");
-const { register, login, getUser } = require("../controllers/authController");
-const authMiddleware = require("../middlewares/authMiddleware");
+const { register, login, getUser, registerDependent } = require("../controllers/authController");
+const authMiddleware = require("../middlewares/auth");
 
 const router = express.Router();
 
-// REGISTER
+// REGISTER (Funder or Caregiver)
 router.post(
   "/register",
   [
-    check("name", "Name is required").not().isEmpty(),
+    check("firstName", "First name is required").not().isEmpty(),
+    check("lastName", "Last name is required").not().isEmpty(),
+    check("surname", "Surname is required").not().isEmpty(),
     check("email", "Include a valid email").isEmail(),
     check("password", "Password must be at least 6 characters").isLength({ min: 6 }),
-    check("role", "Role must be either funder, caregiver, or dependent").isIn(["funder", "caregiver", "dependent"]),
+    check("role", "Role must be either funder or caregiver").isIn(["funder", "caregiver"]),
+    check("Idnumber", "Valid 13-digit numeric ID number required").isLength({ min: 13, max: 13 }).isNumeric(),
   ],
   register
+);
+
+// REGISTER DEPENDENT (by Caregiver)
+router.post(
+  "/register-dependent",
+  [
+    authMiddleware,
+    check("firstName", "First name is required").not().isEmpty(),
+    check("lastName", "Last name is required").not().isEmpty(),
+    check("surname", "Surname is required").not().isEmpty(),
+    check("Idnumber", "Valid 13-digit numeric ID number required").isLength({ min: 13, max: 13 }).isNumeric(),
+    check("relation", "Relation is required").not().isEmpty(),
+  ],
+  registerDependent
 );
 
 // LOGIN
