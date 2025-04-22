ab4d07d1b6a5c0fa85f7990724a767418e5ff998
princemashumu <princemashumu@yahoo.com>
2025-04-22 22:20:33 +0200

    feat. i added a "start" script

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes); // Ensure this is working
 
-// Start the server
+// Start the server 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
