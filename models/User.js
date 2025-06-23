8068f821301c2e7827797e55a3a9f95e558f5cf1
princemashumu <princemashumu@yahoo.com>
2025-06-23 15:57:40 +0200

    feat. suto increment the user ID

--- models/User.js
+++ models/User.js
@@ -2,6 +2,12 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false
+  },
   firstName: {
     type: DataTypes.STRING,
     allowNull: false
